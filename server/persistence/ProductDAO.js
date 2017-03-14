50a8bdb7ce619a1163c5f6e569e887b1866a3543
Samuelson <samuelsonma@gmail.com>
2017-03-14 00:25:58 -0400

    iniciando DAO de produtos

--- server/persistence/ProductDAO.js
+++ server/persistence/ProductDAO.js
@@ -9,7 +9,7 @@ class ProductDAO{
 	}
 
 	buscaPorId(id,callback){
-		this._connection.query('select * from product where id = ?',[id], callback);
+		this._connection.query('select * from product where product_id = ?',[id], callback);
 	}
 
 	salva(product,callback){
@@ -17,7 +17,10 @@ class ProductDAO{
 	}
 
 	atualiza(product,callback){
-		this._connection.query('update product set status = ? where id = ?', [product.status, product.id], callback);
+		this._connection.query('update product set product_name = ?, product_desc = ?, product_qty = ?, product_value = ?, product_category_id = ? where product_id = ?', [product.product_name,product.product_desc,product.product_qty,product.product_value,product.product_categoy_id, product.product_id], callback);
+	}
+	exclui(id, callback){
+		this._connection.query('update product set product_status = 0 where product_id = ?', [id], callback);
 	}
 
 
