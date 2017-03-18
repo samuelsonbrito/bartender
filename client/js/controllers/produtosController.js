18bf021f8e62de84ce122d52f8af53fc8896cc12
Samuelson <samuelsonma@gmail.com>
2017-03-17 21:27:26 -0400

    Iniciando client do projeto

---
+++ client/js/controllers/produtosController.js
@@ -0,0 +1,7 @@
+angular.module('bartender').controller('ProdutosController', function($scope){
+    var foto = {
+        titulo: 'Coca-Cola',
+        foto: 'http://trabalhosujo.com.br/wp/wp-content/uploads/2012/06/coca-cola-logo.jpg'
+    };
+
+});
